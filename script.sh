d1f3570630f088d079ae074ab04f0f28ec37efb5
vkhuy <vkhuy2014vn@gmail.com>
2023-07-19 11:18:31 +0700

    update

--- script.sh
+++ script.sh
@@ -1,23 +1,23 @@
 #!usr/bin/env bash
 
 # Import environment
-bash Module01.sh
+bash Modules/Module01.sh
 
 
 # Quality Control 
 mkdir reads_qc
-bash Module02.sh
+bash Modules/Module02.sh
 mv reads_qc/SRR8278838_sub_fastqc.html ./SRR8278838_sub_fastqc.html
 
 
 # Assemble with trycycler
-bash Module03.sh
+bash Modules/Module03.sh
 # Merge together
 cat trycycler/cluster_*/7_final_consensus.fasta > trycycler.fasta
 
 
 # Long_reads polishing with medaka
-bash Module04.sh
+bash Modules/Module04.sh
 # Clean up
 mv trycycler/cluster_001/medaka/consensus.fasta trycycler/cluster_001/8_medaka.fasta
 rm -r trycycler/cluster_001/medaka trycycler/cluster_001/*.fai trycycler/cluster_001/*.mmi
@@ -28,13 +28,13 @@ cat trycycler/cluster_*/8_medaka.fasta > trycycler_medaka.fasta
 
 
 # Short_reads polishing with polypolish
-bash Module05.sh
+bash Modules/Module05.sh
 rm *.bwt *.pac *.ann *.amb *.sa *.sam
 
 
 # Species classification with kraken2
-bash Module06.sh
+bash Modules/Module06.sh
 
 
 # Annotation with prokka
-bash Module07.sh
+bash Modules/Module07.sh
