4cca61ea9ec4e71a66686a8b37886e0ff8e19347
vkhuy <vkhuy2014vn@gmail.com>
2023-07-21 02:22:30 +0700

    update

--- Modules/Module06.sh
+++ Modules/Module06.sh
@@ -1,4 +1,4 @@
 #!usr/bin/env bash
 
 # Species classification with kraken2
-mamba run -n kraken2 kraken2 --db kraken2/database/library/k2_standard_16gb_20230605 trycycler_medaka_polypolish.fasta  --report kraken.report --use-names
+mamba run -n kraken2 kraken2 --db kraken2/database/library/k2_standard_16gb_20230605 trycycler_medaka_polypolish.fasta  --report kraken.report --use-names > SRR8278838.kraken
--- script.sh
+++ script.sh
@@ -7,7 +7,10 @@ bash Modules/Module01.sh
 # Quality Control 
 mkdir reads_qc
 bash Modules/Module02.sh
-mv reads_qc/SRR8278838_sub_fastqc.html ./SRR8278838_sub_fastqc.html
+mkdir qc_report
+mv fastp.html qc_report/fastp.html
+mv fastp.json qc_report/fastp.json
+mv reads_qc/SRR8278838_sub_fastqc.html qc_report/SRR8278838_sub_fastqc.html
 
 
 # Assemble with trycycler
@@ -31,6 +34,11 @@ cat trycycler/cluster_*/8_medaka.fasta > trycycler_medaka.fasta
 bash Modules/Module05.sh
 rm *.bwt *.pac *.ann *.amb *.sa *.sam
 
+# Move all assembled genomes to assembled folder
+mkdir assembled
+mv trycycler.fasta assembled/trycycler.fasta
+mv trycycler_medaka.fasta assembled/trycycler_medaka.fasta
+mv trycycler_medaka_polypolish.fasta assembled/trycycler_medaka_polypolish.fasta
 
 # Species classification with kraken2
 bash Modules/Module06.sh
@@ -38,3 +46,6 @@ bash Modules/Module06.sh
 
 # Annotation with prokka
 bash Modules/Module07.sh
+mkdir kraken2_report
+mv kraken.report kraken2_report/kraken.report
+mv SRR8278838.kraken kraken2_report/SRR8278838.kraken
